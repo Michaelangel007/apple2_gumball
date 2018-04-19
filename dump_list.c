6a87e4269e0222713db46e587707cd1046cd56b7
Michaelangel007 <michaelangel007@sharedcraft.com>
2018-04-19 09:01:32 -0700

    Use command line filename if specified

--- dump_list.c
+++ dump_list.c
@@ -21,10 +21,13 @@
 
 // Main
 
-    int main()
+    int main( const int nArg, const char *aArg[] )
     {
         const char *name = "gumball.fragment.1.1800";
         FILE       *in   = fopen( name, "rb" );
+
+        if( nArg > 1 )
+            name = aArg[1];       
 
         if( in )
         {
